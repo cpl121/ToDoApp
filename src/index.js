bc4ddc401805fed0269a8bd0e22a16671d8ceb53
cpl121 <cpldev121@gmail.com>
2022-03-16 14:51:46 +0100

    finish app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/index';
 
 
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
+);
+
+ReactDOM.createPortal(
+  <App />,
+  document.getElementById('modal')
 );
